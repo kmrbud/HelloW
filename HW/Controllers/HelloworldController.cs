4a27bbb53ee3a4bcf437d3ba6702d564f301676e
kmrbud <budhathokikmr@outlook.com>
2018-06-22 22:20:30 -0400

    Innitial

---
+++ HW/Controllers/HelloworldController.cs
@@ -0,0 +1,35 @@
+﻿using HW.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HW.Controllers
+{
+    public class HelloworldController : ApiController
+    {
+        static List<Helloworld> helloworld = new List<Helloworld>()
+        {
+            new Helloworld(){ ID = 0, AppName = "Hello World!" },
+            new Helloworld(){ ID = 1, AppName = "Hello" },
+            new Helloworld(){ ID = 2, AppName = "World!" },
+
+        };
+        public IEnumerable<Helloworld> Get()
+        {
+            return helloworld;
+        }
+        public IHttpActionResult Get(int id)
+        {
+            var helloworlds = helloworld.FirstOrDefault((p) => p.ID == id);
+            if (helloworlds == null)
+            {
+                return NotFound();
+            }
+            return Ok(helloworlds);
+        }
+
+    }
+}
---
+++ HW/Models/Helloworld.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW.Models
+{
+    public class Helloworld
+    {
+        
+            public int ID
+            {
+                get;
+                set;
+            }
+            public string AppName
+            {
+                get;
+                set;
+            }
+    }
+}
